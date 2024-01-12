b6719c3d87c7b8e3b795c86d53a9104d9910dcd4
Bestoloc <hinz8amq17x7@mail.ru>
2024-01-12 17:55:20 +0300

    Добавьте файлы проекта.

---
+++ VlozhCycly/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace VlozhCycly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double O = 0;
+            double n = double.Parse(Console.ReadLine());
+            double k = double.Parse(Console.ReadLine());
+            
+            
+            for (int i = 1; i <= n; i++)
+            {
+                double P = 1;
+
+                for (int x = 1; x <= k; x++)
+                {
+                    P = P * Cos(2 * Pow(x, 2) + i);
+                }
+
+                O += P;
+            }
+            Console.WriteLine(O);
+
+
+
+        }
+    }
+}
